f4fedc61d05b162d255587704384813455aab821
Alvin Wang <alvinwang922@gmail.com>
2019-12-30 22:16:27 -0800

    Updated App

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,30 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Staydrated',
       theme: ThemeData(
-        primarySwatch: Colors.cyan,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurple
       ),
-      home: MyHomePage(title: 'Staydrated'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child:
-          Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Your water intake for today as of now is ' + '$_counter'  + ' cup(s).',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            Image.asset(
-                'images/droplet.png'
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Staydrated'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+        body: Column(
+          children: [
+            RaisedButton(
+              onPressed: () {},
+              child: Text('I dont know'),
+            ),
+            ProductManager()
+        ],
+      ),),
     );
   }
 }
